d65a5f43345544885677b1d19f01a0ef0f1abb87
Jonas Myklevold <81618160+jonasmyklevold@users.noreply.github.com>
2022-01-27 14:35:21 +0100

    go.mod

---
+++ goquote.go
@@ -0,0 +1,17 @@
+package gomodule
+
+import "fmt"
+
+import (
+"fmt"
+
+"rsc.io/quote"
+)
+
+func myquote() {
+	fmt.Println(quote.go())
+	fmt.Println(quote.hello())
+	fmt.Println(quote.glass())
+	fmt.Println(quote.opt())
+
+}
