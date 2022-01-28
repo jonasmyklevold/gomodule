2a05e0b20769b955ac644829bd083c256f63242f
Jonas Myklevold <81618160+jonasmyklevold@users.noreply.github.com>
2022-01-28 20:14:37 +0100

    vi får se

--- state/state.go
+++ state/state.go
@@ -1,14 +1,17 @@
 package state
 
+var state = "[kylling rev korn hs ---\\ \\__/ _________________/---]"
+
 func ViewState() string {
 	// Sjekke data som er lagret ("kylling til venstre", "rev til venstre")
-	return "[kylling rev korn hs ---\\ \\__/ _________________/---]"
+	return state
 }
-
-func ViewState2() string {
-	return "[kylling rev korn  ---\\ \\_hs_/ _________________/---]"
+func PutInKylling() {
+	state = "[rev korn hs ---\\ \\_kylling_/ _________________/---]"
 }
-
-func viewState3() string {
-	return "[kylling rev korn  ---\\ \\__/ _________________/---hs]"
+func PutInRev() {
+	state = "[rev korn hs ---\\ \\_Rev_/ _________________/---]"
+}
+func PutInKorn() {
+	state = "[rev korn hs ---\\ \\_Korn_/ _________________/---]"
 }
--- state/state_test.go
+++ state/state_test.go
@@ -1,4 +1,3 @@
-// Package for testing the represention of the state of the riverworld
 package state
 
 import "testing"
@@ -6,7 +5,7 @@ import "testing"
 // Test function implemented in line with the Golang's testing tool
 func TestViewState(t *testing.T) {
 	wanted := "[kylling rev korn hs ---\\ \\__/ _________________/---]"
-	state := ViewState3()
+	state := ViewState()
 	if state != wanted {
 		t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)
 	}
